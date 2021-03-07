ef2eaea104fcdad635de73d211323c7159b21867
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-03-07 15:36:02 -0800

    added cors to index.js

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.use(
   })
 );
 
+//cross origin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
